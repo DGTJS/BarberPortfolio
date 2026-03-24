135e9a5a668182a171ffd206ce0eae78f61cb1f7
DGTJS <joaodiegom15@gmail.com>
2026-03-24 14:07:31 -0300

    feat: implement homepage layout with barbershop listings and bookings

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,63 @@
+import { Header } from "@/app/_components/Header";
+import { SearchInputs } from "@/app/_components/Search-inputs";
+import Banner from "@/public/Banner.png";
 import Image from "next/image";
+import { BookingItem } from "./_components/BookingItem";
+import { BarberShopItem } from "./_components/BarberShopItem";
+import { Footer } from "./_components/Footer";
+import {
+  PageContainer,
+  PageSectionTitle,
+  PageSection,
+  PageSectionScroller,
+} from "./_components/ui/page";
 
-export default function Home() {
+import { prisma } from "@/lib/prisma";
+
+export default async function Home() {
+  const barberShops = await prisma.barberShop.findMany();
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+      <Header />
+      <PageContainer>
+        <SearchInputs />
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src={Banner}
+          alt="Agendar Agora!"
+          sizes="100vw"
+          className="h-auto w-full "
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        <PageSection>
+          <PageSectionTitle>Agendamentos</PageSectionTitle>
+          <BookingItem
+            serviceName="Corte de Cabelo"
+            barberShopName="Barbearia do João"
+            barberShopImage="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+            date={new Date()}
+          />
+        </PageSection>
+        <PageSection>
+          <PageSectionTitle>Barbearias</PageSectionTitle>
+          <PageSectionScroller>
+            {barberShops.map((barberShop) => {
+              return (
+                <BarberShopItem key={barberShop.id} barberShop={barberShop} />
+              );
+            })}
+          </PageSectionScroller>
+        </PageSection>
+        <PageSection>
+          <PageSectionTitle>Populares</PageSectionTitle>
+          <PageSectionScroller>
+            {barberShops.map((barberShop) => {
+              return (
+                <BarberShopItem key={barberShop.id} barberShop={barberShop} />
+              );
+            })}
+          </PageSectionScroller>
+        </PageSection>
+      </PageContainer>
+      <Footer />
+    </>
   );
 }
